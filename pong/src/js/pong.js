3f66f5446e15be8b7dc9e3c63ce6a4dc2dadf22f
Sasha <suligvondooka@gmail.com>
2025-12-15 19:52:46 +0100

    Surprise -- nothing yet, only added React & three libs..

--- pong/src/js/pong.js
+++ pong/src/js/pong.js
@@ -3,3 +3,8 @@
 /* **********************************************/
 import { GetJSONdata } from "./fetch.js";
 
+const canvas = document.getElementById("gm-canvas");
+const ctx = canvas.getContext("2d");
+
+
+
