97a4ec7f3a2579bebeb50a940a99c3cb582596a4
Sasha Domínguez Ferrer <sadoming@student.42barcelona.com>
2025-12-17 20:00:38 +0100

    Removed unecessary framework, i will do it in pure js for now

--- js/pong.js
+++ js/pong.js
@@ -3,19 +3,45 @@
 /* **********************************************/
 import { GetJSONdata } from "./fetch.js";
 
-const canvas = document.getElementById("gm-canvas");
+/** PC -- gmscale */
+const GAME_WIDTH	= 800;
+const GAME_HEIGHT	= 600;
+
+const canvas = document.getElementById("gm-canvas");	// The game canvas
 const ctx = canvas.getContext("2d");
 
 /* better render */
+/*
 const dpr = window.devicePixelRatio || 1;
 const cn_rect = canvas.getBoundingClientRect();
 
-//canvas.width = cn_rect.width * dpr;		// 2420
-//canvas.height = cn_rect.height * dpr;	// 1120
+canvas.width = cn_rect.width * dpr;		// 2420
+canvas.height = cn_rect.height * dpr;	// 1120
 
 ctx.scale(dpr, dpr);
+*/
 ctx.imageSmoothingEnabled = true;
 ctx.imageSmoothingQuality = "high";
+
+function resizeCanvas() {
+	const scale = Math.min(
+	  window.innerWidth / GAME_WIDTH,
+	  window.innerHeight / GAME_HEIGHT
+	);
+
+	canvas.style.width = GAME_WIDTH * scale + "px";
+	canvas.style.height = GAME_HEIGHT * scale + "px";
+
+	const dpr = window.devicePixelRatio || 1;
+	canvas.width = GAME_WIDTH * dpr;
+	canvas.height = GAME_HEIGHT * dpr;
+
+	ctx.setTransform(dpr, 0, 0, dpr, 0, 0);
+	ctx.scale(scale, scale);
+}
+
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
 //*********** */
 
 /* ball texture */
